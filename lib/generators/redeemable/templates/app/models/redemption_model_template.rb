c70e1e72b29f5192552d8a0c1b337c86992af31f
Calvin Yu <csyu77@gmail.com>
2009-09-02 10:08:16 -0400

    fix counter cache for redemptions

--- lib/generators/redeemable/templates/app/models/redemption_model_template.rb
+++ lib/generators/redeemable/templates/app/models/redemption_model_template.rb
@@ -1,5 +1,5 @@
 class <%= redemption_class_name %> < ActiveRecord::Base
-  belongs_to :<%= singular_name %>, :counter_cache => true
+  belongs_to :<%= singular_name %>, :counter_cache => 'redemptions_count'
   belongs_to :user
 
   validates_presence_of :user_id, :<%= singular_name %>_id
